ee48cf295c85054beaf504091dab48293f8713f7
Cesar Deltoro <cesardeltoro@Cesars-MacBook-Air.local>
2021-12-01 19:22:11 -0600

    Fixed proper answer count at the end of questions prompted to show the ammount of correct answers answered.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # about-me
 code fellows 201
+
+about-me
+
+Lab-03
+
+This lab we were assigned to create a guessing game and give ther user 4 attempts by prompting them to guess a number from 1-10 and if they do not guess the number after 4 attemps they will be prompted with the correct answer. Also inserting an array and using that array to carry answers inside of it and with that array ill use a loop and give the user 6 attemps to guess the answer and if they don't get it correct they are prompted with an alert after the 6 tries that they're attempts are up and the correct color is (inserted) along with all of the other options.
+
--- js/apps.js
+++ js/apps.js
@@ -6,6 +6,7 @@ alert('Thanks for visiting the site ' + intro);
 
 alert('For these Questions please answer y or n');
 
+let correctNumber = 0;
 
 // Question 1
 let userAnswer1 = prompt('Am I from the west coast?').toLowerCase();
@@ -14,6 +15,7 @@ let userAnswer1 = prompt('Am I from the west coast?').toLowerCase();
 if (userAnswer1 === 'y' || userAnswer1 === 'yes'){
   // console.log('Wow you actually read it!');
   alert('Correct, I am from the west coast!');
+  correctNumber++ ;
 }else if(userAnswer1 === 'n' || userAnswer1 === 'no'){
   // console.log();
   alert('Wrong, I actually am from the west coast');
@@ -26,6 +28,7 @@ let userAnswer2 = prompt('Did I go to college?').toLowerCase();
 if(userAnswer2 === 'y' || userAnswer2 ==='yes'){
   // console.log('Ok I did go to college')
   alert('Correct, I did go to college');
+  correctNumber++ ;
 }else if(userAnswer2 === 'n' || userAnswer2 === 'no'){
   // console.log('It worked')
   alert('Wrong, I actually did go to college');
@@ -37,6 +40,7 @@ let userAnswer3 = prompt('Did I join the military').toLowerCase();
 if(userAnswer3 === 'y' || userAnswer3 === 'yes'){
   // console.log()
   alert('Correct, I did join the military');
+  correctNumber++ ;
 } else if( userAnswer3 === 'n' || userAnswer3 === 'no'){
   alert('Wrong, I did join the military');
 }
@@ -50,6 +54,7 @@ if(userAnswer4 === 'y' || userAnswer4 ==='yes'){
 }else if(userAnswer4 === 'n' || userAnswer4 ==='no'){
   // console.log();
   alert('Correct, its Sky Blue..');
+  correctNumber++ ;
 }
 
 // askQuestions 5
@@ -58,6 +63,7 @@ let userAnswer5 = prompt('Do I like the dish chicken Tikka Masala?').toLowerCase
 if(userAnswer5 === 'y' || userAnswer5 === 'yes'){
   // console.log();
   alert('Correct, That is one of my favorite dishes!');
+  correctNumber++ ;
 }else if(userAnswer5 === 'n' || userAnswer5 === 'no'){
   // console.log();
   alert('Wrong, it actually is one of my favorite dishes.');
@@ -68,9 +74,11 @@ let numberQuestion = prompt('Welcome to my guessing game. Guess a number from 1-
 let correctAnswer = 4;
 let tries = 4;
 
-for(let i = 1; i < tries; i++){
-  if( numberQuestion === correctAnswer){
+for(let i = 0; i < tries; i++){
+  // eslint-disable-next-line eqeqeq
+  if( numberQuestion == correctAnswer){
     alert('CONGRATS THATS CORRECT');
+    correctNumber++ ;
     break;
   }
   else if( numberQuestion > correctAnswer){
@@ -79,21 +87,24 @@ for(let i = 1; i < tries; i++){
   else if (numberQuestion < correctAnswer){
     numberQuestion= prompt('Too Low Please Try Again');
   }
-  if (i === tries-1){
+  if (i === 3){
     alert('You have reached the ammount of tries the correct answer was ' + correctAnswer);
   }
 }
+
 let colorArray = ['blue', 'sky blue', 'pink', 'purple', 'black', 'green'];
 let colorGame = prompt('What is my favorite color');
 let triess = 6;
-let answerCorrectly = false;
 
-// for( let i = 0; i < triess; i++)
-while(!answerCorrectly){
-  if( colorArray[1]== colorGame){
-    alert('Congrats that was correct!');
-    break; }
-  else if(colorGame !== colorArray[1]){
-    colorGame = prompt('Incorrect, try again you have 5 more attempts');}
+for( let i = 0; i < triess; i++){
+  if(colorGame === colorArray[1]){
+    alert('Congrats that was the correct Answer');
+    correctNumber++ ;
+    break;
+  }else if(colorGame !== colorArray[1]){
+    colorGame = prompt('Thats not quite right try again');
+  }if(i === 5){
+    alert('Attempts are up the correct answer was ' + colorArray[1] +'. These were all of the options that were listed inside of the array ' + colorArray);
+  }
 }
-
+alert('You got ' + correctNumber + ' of my COMPLEX questions correct');
